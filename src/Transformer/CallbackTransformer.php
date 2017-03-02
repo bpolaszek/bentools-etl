51d276b132299d9e1ea46cd35a19cf11bff2fe52
Beno!t POLASZEK <bpolaszek@gmail.com>
2017-03-02 16:57:33 +0100

    New CallbackTransformer

---
+++ src/Transformer/CallbackTransformer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BenTools\ETL\Transformer;
+
+use BenTools\ETL\Context\ContextElementInterface;
+
+class CallbackTransformer implements TransformerInterface
+{
+    /**
+     * @var callable
+     */
+    private $callback;
+
+    /**
+     * CallbackTransformer constructor.
+     * @param callable $callback
+     */
+    public function __construct(callable $callback)
+    {
+        $this->callback = $callback;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function __invoke(ContextElementInterface $element): void
+    {
+        $callback = $this->callback;
+        $element->setData($callback($element->getData()));
+    }
+}
