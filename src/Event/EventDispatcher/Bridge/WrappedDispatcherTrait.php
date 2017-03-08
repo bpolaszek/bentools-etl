02ce8f8a7c2bae0c3a09b44fe1a0a328fbea46d4
Beno!t POLASZEK <bpolaszek@gmail.com>
2017-03-08 11:36:43 +0100

    Fix PHPDoc type hint

--- src/Event/EventDispatcher/Bridge/WrappedDispatcherTrait.php
+++ src/Event/EventDispatcher/Bridge/WrappedDispatcherTrait.php
@@ -2,12 +2,11 @@
 
 namespace BenTools\ETL\Event\EventDispatcher\Bridge;
 
-use Symfony\Component\EventDispatcher\EventDispatcherInterface as SymfonyEventDispatcherInterface;
 
 trait WrappedDispatcherTrait
 {
     /**
-     * @var SymfonyEventDispatcherInterface
+     * @var mixed
      */
     protected $wrappedDispatcher;
 
@@ -21,7 +20,7 @@ trait WrappedDispatcherTrait
     }
 
     /**
-     * @return SymfonyEventDispatcherInterface
+     * @return mixed
      */
     public function getWrappedDispatcher()
     {
