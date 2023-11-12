1da9f77ccfbc89af79f911f2c8f1c90d6e5c7e9d
Beno!t POLASZEK <bpolaszek@gmail.com>
2023-11-12 09:10:20 +0100

    feat: multiple next tick callbacks

--- tests/Behavior/NextTickTest.php
+++ tests/Behavior/NextTickTest.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace BenTools\ETL\Tests\Behavior;
 
+use ArrayObject;
 use BenTools\ETL\EtlConfiguration;
 use BenTools\ETL\EtlExecutor;
 use BenTools\ETL\EtlState;
 use BenTools\ETL\EventDispatcher\Event\ExtractEvent;
+use BenTools\ETL\EventDispatcher\Event\LoadEvent;
 use BenTools\ETL\Tests\InMemoryLoader;
 
 use function expect;
@@ -34,4 +36,22 @@ it('does arbitrary stuff on next tick', function () {
         ->and($report->output)->toHaveCount(2)
         ->and($report->output[0])->toBe(['banana', 'apple'])
         ->and($report->output[1])->toBe(['strawberry', 'raspberry', 'peach']);
+});
+
+it('can trigger several callbacks, which are called only once', function () {
+    // Given
+    $bucket = new ArrayObject();
+    $etl = (new EtlExecutor())
+        ->onLoad(function (LoadEvent $event) use ($bucket) {
+            if ('apple' === $event->item) {
+                $event->state->nextTick(fn (EtlState $state) => $bucket->append('apple'));
+                $event->state->nextTick(fn (EtlState $state) => $bucket->append('APPLE'));
+            }
+        });
+
+    // When
+    $report = $etl->process(['banana', 'apple', 'strawberry', 'raspberry', 'peach']);
+
+    // Then
+    expect([...$bucket])->toBe(['apple', 'APPLE']);
 });
